a396ae6eb147a114dcf96aa54c55bd8b77d80ada
criztiandev <criztiandev@gmail.com>
2024-09-30 07:49:10 +0800

    Added Product Details

--- src/app/product/details/[id].tsx
+++ src/app/product/details/[id].tsx
@@ -1,13 +1,18 @@
 import YStack from "@/components/stacks/YStack";
 import BaseLayout from "@/layout/BaseLayout";
-import React from "react";
+import React, { useState } from "react";
 import ProductHero from "./components/ProductHero";
 import ProductAddons from "./components/ProductAddons";
 import ProductQuantity from "./components/ProductQuantity";
 import ProductActions from "./components/ProductActions";
 import ProductHeader from "./components/ProductHeader";
+import useCartStore from "@/state/useCartStore";
+import useProductStore from "@/state/useProductStore";
 
 const RootScreen = () => {
+  const { getProductDetails } = useCartStore();
+  const [quantity, setQuantity] = useState<number>(0);
+
   return (
     <>
       <BaseLayout>
@@ -15,8 +20,18 @@ const RootScreen = () => {
         <YStack className="px-2">
           <ProductHero />
           <ProductAddons />
-          <ProductQuantity />
-          <ProductActions />
+          <ProductQuantity quantity={quantity} setQuantity={setQuantity} />
+          <ProductActions
+            product={{
+              id: "123123",
+              name: "Product 1",
+              price: 3000,
+              rating: 5.0,
+              thumbnailUrl: "12312312312",
+              addons: [],
+            }}
+            quantity={quantity}
+          />
         </YStack>
       </BaseLayout>
     </>
