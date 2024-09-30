827654f6b20b9b3b50cba5851f9b38fb133dd9f6
criztiandev <criztiandev@gmail.com>
2024-09-30 14:26:37 +0800

    Remove: remove all the unecessary import

--- src/app/product/_layout.tsx
+++ src/app/product/_layout.tsx
@@ -1,7 +1,5 @@
-import { View, Text } from "react-native";
-import React, { useEffect } from "react";
-import { Stack, Tabs } from "expo-router";
-import { useAuth } from "@/providers/AuthProvider";
+import React from "react";
+import { Stack } from "expo-router";
 import ProtectedRoute from "@/components/routes/ProtectedRoute";
 
 const RootLayout = () => {
