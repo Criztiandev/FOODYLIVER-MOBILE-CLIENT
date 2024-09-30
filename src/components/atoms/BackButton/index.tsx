29f7344199745205ac887316e10b8dc94c007e9c
criztiandev <criztiandev@gmail.com>
2024-09-30 08:42:44 +0800

    Added Class

--- src/components/atoms/BackButton/index.tsx
+++ src/components/atoms/BackButton/index.tsx
@@ -6,7 +6,7 @@ import { TouchableOpacity } from "react-native";
 const BackButton = () => {
   const router = useRouter();
   return (
-    <TouchableOpacity onPress={() => router.back()}>
+    <TouchableOpacity onPress={() => router.back()} className="mr-4">
       <ChevronLeft color="black" />
     </TouchableOpacity>
   );
