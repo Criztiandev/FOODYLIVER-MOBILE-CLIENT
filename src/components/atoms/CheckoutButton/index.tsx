6a834c15b99f763e24c825b93e9d2d92f5ab341a
criztiandev <criztiandev@gmail.com>
2024-09-30 14:27:08 +0800

    Add: navigation to the delivery

--- src/components/atoms/CheckoutButton/index.tsx
+++ src/components/atoms/CheckoutButton/index.tsx
@@ -1,12 +1,14 @@
 import XStack from "@/components/stacks/XStack";
 import Button from "@/components/ui/Button";
+import { useRouter } from "expo-router";
 import { ShoppingCart } from "lucide-react-native";
 import React from "react";
 import { Text } from "react-native";
 
 const CheckoutButton = () => {
+  const router = useRouter();
   return (
-    <Button className="my-8">
+    <Button className="my-8" onPress={() => router.push("/cart/delivery")}>
       <XStack className="space-x-4">
         <ShoppingCart color="white" />
         <Text className="text-white text-lg">Checkout Order</Text>
