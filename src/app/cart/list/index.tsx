bdea43b048fd57618d7b1da19f619218bf465a83
criztiandev <criztiandev@gmail.com>
2024-09-30 12:12:48 +0800

    Clean: remove up the unecessary imports

--- src/app/cart/list/index.tsx
+++ src/app/cart/list/index.tsx
@@ -1,30 +1,14 @@
 import BackButton from "@/components/atoms/BackButton";
-import XStack from "@/components/stacks/XStack";
 import YStack from "@/components/stacks/YStack";
-import Avatar from "@/components/ui/Avatar";
 import Button from "@/components/ui/Button";
 import BaseLayout from "@/layout/BaseLayout";
 import useCartStore from "@/state/useCartStore";
-import { FlashList } from "@shopify/flash-list";
 import { Stack, useRouter } from "expo-router";
-import {
-  Check,
-  ChevronLeft,
-  Minus,
-  Phone,
-  Pin,
-  Plus,
-  ShoppingCart,
-  User,
-  View,
-} from "lucide-react-native";
+import { Plus } from "lucide-react-native";
 import React from "react";
-import { Text, TouchableOpacity } from "react-native";
+import { Text } from "react-native";
 import SelectedProducts from "./components/CartSelectedProduct";
-import CustomerDetails from "./components/CartCustomerDetails";
 import CartCalculation from "./components/CartCalculation";
-import CheckoutButton from "@/components/atoms/CheckoutButton";
-import CartAddons from "./components/CartAddons";
 import CartEmpty from "./components/CartEmpty";
 import PaymentButton from "@/components/atoms/PaymentButton";
 
