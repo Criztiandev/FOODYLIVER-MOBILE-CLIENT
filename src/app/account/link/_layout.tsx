21f019086843cacafe4bcfdf1955343adf99b5b7
criztiandev <criztiandev@gmail.com>
2024-10-01 12:25:17 +0800

    Added Account Link route

---
+++ src/app/account/link/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from "expo-router";
+import React from "react";
+
+const RootScreen = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="[id]" />
+    </Stack>
+  );
+};
+
+export default RootScreen;
