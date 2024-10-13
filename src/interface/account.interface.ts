05610cbce25eeab0d14c355e604774281ac42d7b
criztian <criztiandev@gmail.com>
2024-10-13 20:03:27 +0800

    refactor: typescript promise based for user and unknown

--- src/interface/account.interface.ts
+++ src/interface/account.interface.ts
@@ -1,12 +1,10 @@
-import { UseMutationResult } from "@tanstack/react-query";
 import { User } from "./user.interface";
 
 export interface AccountStore {
   credentials: User | null;
 
   logout: () => void;
-  getCredentials: () => void;
+  getCredentials: () => Promise<User | unknown>;
   setCredentials: (credentials: User) => void;
   updateCredentials: (credentials: User) => void;
-  deleteAccount: () => UseMutationResult<any, Error>;
 }
