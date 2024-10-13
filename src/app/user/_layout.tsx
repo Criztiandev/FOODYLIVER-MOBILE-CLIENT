5b2aea4c1e29ecbeb3a350254e00b3b9fb9cb77f
criztian <criztiandev@gmail.com>
2024-10-13 19:55:11 +0800

    chores: remove unecessary import and component

--- src/app/user/_layout.tsx
+++ src/app/user/_layout.tsx
@@ -1,16 +1,11 @@
-import { View, Text } from "react-native";
-import React, { useEffect } from "react";
-import { Stack, Tabs } from "expo-router";
-import { useAuth } from "@/providers/AuthProvider";
-import ProtectedRoute from "@/components/routes/ProtectedRoute";
+import React from "react";
+import { Stack } from "expo-router";
 
 const RootLayout = () => {
   return (
-    <ProtectedRoute allowedRoles={["user"]}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tab)" />
-      </Stack>
-    </ProtectedRoute>
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(tab)" />
+    </Stack>
   );
 };
 
