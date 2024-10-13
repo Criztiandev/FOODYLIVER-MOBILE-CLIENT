25a82a2cb7c9b394891c80b9183828d2365232d3
criztian <criztiandev@gmail.com>
2024-10-13 20:02:18 +0800

    feat: added useLogout hook

--- src/hooks/account/useLogout.ts
+++ src/hooks/account/useLogout.ts
@@ -1,14 +1,28 @@
 import useMutate from "../query/useMutate";
 import { PrivateAxios } from "@/lib/axios";
+import useAccountStore from "@/state/useAccountStore";
 import { useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "expo-router";
+import useLocalStorage from "../utils/useLocalStorage";
 
 const useLogout = () => {
+  const router = useRouter();
+  const { removeItem } = useLocalStorage();
   const queryClient = useQueryClient();
   return useMutate({
-    mutationKey: ["DELETE /logout"],
-    mutationFn: () => PrivateAxios.delete("/"),
-    onSuccess: () => {
-      queryClient.clear();
+    mutationKey: ["POST /logout"],
+    mutationFn: () => PrivateAxios.post("/logout"),
+    onSuccess: (data) => {
+      removeItem("user");
+      removeItem("accessToken");
+
+      router.dismissAll();
+      queryClient.removeQueries();
+      router.replace("/auth/sign-in");
+    },
+
+    onError: (e) => {
+      console.log(e);
     },
   });
 };
