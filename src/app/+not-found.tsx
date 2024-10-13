8d347464f8a8671476258cf9940efd771b22d3e1
criztian <criztiandev@gmail.com>
2024-10-13 19:52:18 +0800

    design: updated the not found design

--- src/app/+not-found.tsx
+++ src/app/+not-found.tsx
@@ -2,9 +2,5 @@ import { Stack } from "expo-router";
 
 export default function NotFoundScreen() {
   console.log("Not Founnd");
-  return (
-    <>
-      <Stack.Screen options={{ title: "Oops!" }} />
-    </>
-  );
+  return <Stack.Screen options={{ title: "Oops!" }} />;
 }
